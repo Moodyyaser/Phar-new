c69d064857bafa6b82fa9f3058d994234a16160d
Moodyyaser <moodyyaser0@gmail.com>
2021-09-23 20:20:37 +0200

    commit

---
+++ src/app/elements.component.ts
@@ -0,0 +1,27 @@
+import { PeriodicElement, PharmaticElement } from './elements.model';
+
+export const ELEMENT_DATA: PeriodicElement[] = [];
+
+export const ELEMENT_SALES: PharmaticElement[] = [];
+
+/* dummy data
+{id: 1 , name: 'Hydrogen'  , weight: 1.0079 , amount: [2,3,2], price: 2.50 },
+{id: 2 , name: 'Helium'    , weight: 4.0026 , amount: [1,2,4], price: 20.13},
+{id: 3 , name: 'Lithium'   , weight: 6.941  , amount: [1,0,2], price: 4.24 },
+{id: 4 , name: 'Beryllium' , weight: 9.0122 , amount: [2,0,4], price: 5.45 },
+{id: 5 , name: 'Boron'     , weight: 10.811 , amount: [1,2,3], price: 10.23},
+{id: 6 , name: 'Carbon'    , weight: 12.0107, amount: [4,3,3], price: 1    },
+{id: 7 , name: 'Nitrogen'  , weight: 14.0067, amount: [2,4,3], price: 2.00 },
+{id: 8 , name: 'Oxygen'    , weight: 15.9994, amount: [0,1,0], price: 50   },
+{id: 9 , name: 'Fluorine'  , weight: 18.9984, amount: [1,1,3], price: 54.15},
+{id: 10, name: 'Neon'      , weight: 20.1797, amount: [1,2,3], price: 5.81 },
+{id: 11, name: 'Sodium'    , weight: 22.9898, amount: [3,2,4], price: 34.6 },
+{id: 12, name: 'Magnesium' , weight: 24.305 , amount: [3,4,2], price: 4.63 },
+{id: 13, name: 'Aluminum'  , weight: 26.9815, amount: [0,4,1], price: 2.87 },
+{id: 14, name: 'Silicon'   , weight: 28.0855, amount: [3,0,1], price: 2.13 },
+{id: 15, name: 'Phosphorus', weight: 30.9738, amount: [2,0,2], price: 60   },
+{id: 16, name: 'Sulfur'    , weight: 32.065 , amount: [4,2,1], price: 0.04 },
+{id: 17, name: 'Chlorine'  , weight: 35.453 , amount: [4,2,2], price: 3.33 },
+{id: 18, name: 'Argon'     , weight: 39.948 , amount: [2,0,4], price: 0.11 },
+{id: 19, name: 'Potassium' , weight: 39.0983, amount: [0,2,0], price: 4.13 },
+{id: 20, name: 'Calcium'   , weight: 40.078 , amount: [0,0,3], price: 0.34 },*/
---
+++ src/app/elements.model.ts
@@ -0,0 +1,15 @@
+export interface PeriodicElement {
+  id:     number;
+  name:   string;
+  weight: number;
+  amount: number;
+  price:  number;
+}
+
+export interface PharmaticElement {
+  id:        number;
+  name:      string;
+  capsulent: number;
+  tablets:   number;
+  price:     number;
+}
