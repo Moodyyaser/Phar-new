6c9860488eb30aa9ae9a40c87b4cbe60fd4da3e3
Moodyyaser <moodyyaser0@gmail.com>
2021-09-29 23:49:13 +0200

    added servers

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const postsRoutes = require("./routes/posts");
@@ -8,28 +9,28 @@ const userRoutes = require("./routes/user");
 const app = express();
 
 //Connect to mongoose
-mongoose
-    .connect(
-        "mongodb+srv://max:1tzPJy6TXyeVyLSs@cluster0.kdtdt.mongodb.net/Element_data"
-    )
-    .then(() => {
-        console.log("Connected to database!");
-    })
-    .catch(() => {
-        console.log("Connection failed! (check the IP address)");
-    });
+mongoose.connect("mongodb+srv://max:1tzPJy6TXyeVyLSs@cluster0.kdtdt.mongodb.net/node-angular")
+.then(() => {
+  console.log('Connected to database!');
+})
+.catch(() => {
+  console.log('Connection failed! (check the IP address)');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  next();
 });
 
 app.use("/api/posts", postsRoutes);
