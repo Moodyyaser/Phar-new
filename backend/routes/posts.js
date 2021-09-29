15758838a83bb1cb73372e2065b9e4a4bd641990
Moodyyaser <moodyyaser0@gmail.com>
2021-09-30 00:06:16 +0200

    removed multer

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const multer = require("multer");
 const Post = require('../models/post');
 const checkAuth = require("../middleware/check-auth");
 
@@ -10,23 +9,6 @@ const MIME_TYPE_MAP = {
   'image/jpeg': 'jpg',
   'image/jpg': 'jpg'
 }
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) error = "null";
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    console.log("tried here")
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  }
-});
-
-var upload = multer({storage: storage}).single("image");
 
 //Create post
 router.post("", checkAuth, upload, (req, res, next) => {
